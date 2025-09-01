122a044ab0f4d1ae9d38903d1b33ced4b7b1359e
Laurachristine <lhaurah313@gmail.com>
2025-09-02 01:33:14 +0300

    Added view, create, edit and delete to Facility Controller

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -33,7 +33,7 @@ class FacilityController extends Controller
      */
     public function create()
     {
-        //
+        return view('facilities.create');
     }
 
     /**
@@ -41,7 +41,17 @@ class FacilityController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+           'name' => 'required|string|max:255',
+           'location' => 'nullable|string|max:255',
+           'description' => 'nullable|string',
+           'partner_organization' => 'nullable|string|max:255',
+           'facility_type' => 'nullable|string|max:255',
+           'capabilities' => 'nullable|string',
+        ]);
+
+        Facility::create($validated);
+        return redirect()->route('facilities.index')->with('success', 'Facility registered successfully.');
     }
 
     /**
@@ -49,7 +59,7 @@ class FacilityController extends Controller
      */
     public function show(Facility $facility)
     {
-        //
+        return view('facilities.show', compact('facility'));
     }
 
     /**
@@ -57,7 +67,7 @@ class FacilityController extends Controller
      */
     public function edit(Facility $facility)
     {
-        //
+        return view('facilities.edit', compact('facility'));
     }
 
     /**
@@ -65,7 +75,17 @@ class FacilityController extends Controller
      */
     public function update(Request $request, Facility $facility)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'location' => 'nullable|string|max:255',
+            'description' => 'nullable|string',
+            'partner_organization' => 'nullable|string|max:255',
+            'facility_type' => 'nullable|string|max:255',
+            'capabilities' => 'nullable|string',
+        ]);
+
+        $facility->update($validated);
+        return redirect()->route('facilities.index')->with('success', 'Facility updated successfully.');
     }
 
     /**
@@ -73,6 +93,7 @@ class FacilityController extends Controller
      */
     public function destroy(Facility $facility)
     {
-        //
+        $facility->delete();
+        return redirect()->route('facilities.index')->with('success', 'Facility deleted successfully.');
     }
 }
