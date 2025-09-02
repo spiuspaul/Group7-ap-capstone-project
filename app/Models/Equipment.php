03b97bdfbc7a03a44d9ac1ec7dc164e27c07f5de
Jovia Joyce <joviajoyce7@gmail.com>
2025-09-02 10:41:24 +0300

    equipment

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipment extends Model
 {
-    //
+    Use HasFactory;
+
+    protected $fillable = ['name','description', 'facility-id'];
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+        
+    }
 }
