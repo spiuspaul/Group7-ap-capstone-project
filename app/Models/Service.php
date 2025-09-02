776f0dcdd4a1633a46688710d3166b7acf47390b
Jovia Joyce <joviajoyce7@gmail.com>
2025-09-02 10:36:39 +0300

    service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name','description','facility_id'];
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
 }
